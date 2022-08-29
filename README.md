b533551048a62047e2075914bb46f5c1fcf02213
koopa1338 <sinner1991@gmail.com>
2022-08-29 21:53:55 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+## RiSuto
+
+🚧  NOTE: Early development 🚧
+
+TUI application for interacting with [anilist](https://anilist.co/)
+
+## License
+
+[MIT License](./LICENSE.md)
---
+++ src/main.rs
@@ -0,0 +1,184 @@
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use thiserror::Error;
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
+
+enum InputMode {
+    Normal,
+    Insert,
+    Visual,
+}
+
+/// App holds the state of the application
+struct App {
+    /// Current value of the input box
+    search: String,
+    /// Current input mode
+    input_mode: InputMode,
+    /// History of recorded messages
+    results: Vec<String>,
+}
+
+impl Default for App {
+    fn default() -> App {
+        App {
+            search: String::new(),
+            input_mode: InputMode::Normal,
+            results: Vec::new(),
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+enum AppError {
+    #[error("Something went wrong with io...")]
+    IOError(#[from] std::io::Error),
+}
+
+type Result<T> = std::result::Result<T, AppError>;
+
+fn main() -> Result<()> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = std::io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let app = App::default();
+    let res = run_app(&mut terminal, app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match app.input_mode {
+                InputMode::Normal => match key.code {
+                    KeyCode::Char('i') => {
+                        app.input_mode = InputMode::Insert;
+                    }
+                    KeyCode::Char('q') => {
+                        return Ok(());
+                    }
+                    _ => {}
+                },
+                InputMode::Insert => match key.code {
+                    KeyCode::Enter => {
+                        app.results.push(app.search.drain(..).collect());
+                    }
+                    KeyCode::Char(c) => {
+                        app.search.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        app.search.pop();
+                    }
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    }
+                    _ => {}
+                },
+                InputMode::Visual => todo!(),
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Length(3),
+                Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+
+    let (msg, style) = match app.input_mode {
+        InputMode::Normal => (
+            vec![
+                Span::raw("Press "),
+                Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to exit, "),
+                Span::styled("i", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to start editing."),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        ),
+        InputMode::Insert => (
+            vec![
+                Span::raw("Press "),
+                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to stop editing, "),
+                Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to record the message"),
+            ],
+            Style::default(),
+        ),
+        InputMode::Visual => todo!(),
+    };
+    let mut text = Text::from(Spans::from(msg));
+    text.patch_style(style);
+    let help_message = Paragraph::new(text);
+    f.render_widget(help_message, chunks[0]);
+
+    let input = Paragraph::new(app.search.as_ref())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Insert => Style::default().fg(Color::Yellow),
+            InputMode::Visual => todo!(),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Input"));
+    f.render_widget(input, chunks[1]);
+    match app.input_mode {
+        InputMode::Normal => {}
+        InputMode::Insert => {
+            f.set_cursor(chunks[1].x + app.search.width() as u16 + 1, chunks[1].y + 1)
+        }
+        InputMode::Visual => todo!(),
+    }
+
+    let messages: Vec<ListItem> = app
+        .results
+        .iter()
+        .enumerate()
+        .map(|(i, m)| {
+            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+            ListItem::new(content)
+        })
+        .collect();
+    let messages = List::new(messages).block(Block::default().borders(Borders::ALL).title("List"));
+    f.render_widget(messages, chunks[2]);
+}
